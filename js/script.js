bdd2cad6135e7e4f1bdeb175fb34af3f3cbe831e
chayan999 <chayanchandandas@gmail.com>
2025-02-23 19:11:31 +0600

    add footer section and small device responsive

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
 
 
-let myArr = ["chayan", "chandan", "das", 22, "10"];
-console.log(myArr)
+// let myArr = ["chayan", "chandan", "das", 22, "10"];
+// console.log(myArr)
 
-let result = myArr.toString();
+// let result = myArr.toString();
 
-console.log(result)
+// // console.log(result)
+
+// let age = 20;
+// let salary = 50000000;
+// let job = true;
+
+// if(age>=18){
+//     if(job){
+//         if(salary>=50000){
+//             console.log('Yes you can marrage')
+//         }
+//         else{
+//             console.log("You can't marrage your salary is low")
+//         }
+//     }else{
+//         console.log("no job no marrage")
+//     }
+// }else{
+//     console.log("Your can't marrage your age is low")
+// }
