a3d38cfc89d7760a5fa2e0cc435436eb37668483
chayan999 <chayanchandandas@gmail.com>
2025-02-25 21:50:27 +0600

    add md device responsive banner and information section

--- js/script.js
+++ js/script.js
@@ -25,3 +25,26 @@
 // }else{
 //     console.log("Your can't marrage your age is low")
 // }
+
+// let test = document.querySelector(".test")
+
+// let inner = test.innerHTML= "<li> hi</li>"
+// console.log(inner)
+
+//  function test(){
+//    let a = 10;
+//    let b = 20;
+//    console.log(a+b)
+// }
+// let x = test()
+// console.log(test())
+// console.log(x)
+
+// console.warn("thiss is warn")
+// console.error("this is error")
+// console.info("this is info")
+// console.table([{name:"Das",age:25},{name:"Chayan", age:23}])
+// // console.clear()
+// console.group('group1')
+// console.log("Inside Group1")
+// console.groupEnd()
